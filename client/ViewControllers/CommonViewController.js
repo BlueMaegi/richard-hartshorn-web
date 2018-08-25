a3c9f63541c21a505cb805e9bc2ab43b6818082e
Tami Hartshorn <thartshorn42@gmail.com>
2018-08-25 18:14:31 -0400

    Add common functions and styles

--- client/ViewControllers/CommonViewController.js
+++ client/ViewControllers/CommonViewController.js
@@ -1,32 +1,70 @@
 var ajaxLoadStack = [];
+var defaultPageSize = 10;
 
 $(function(){
-	//if we don't have a cookie yet, create a default one
-	//if(GetCookie() == null)
-	//	SetCookie({});
-
-
-	$(window).on("loadscripts", function(){
-		var width = 0; 
-		$('.navlinks a').each(function(i, ele){
-			var textW = $(ele).width();
-			var marginW = $(ele).css("margin-right").replace("%", "").replace("px", "");
-			width += textW + ($('body').width() * (marginW/100.0));
-		});
-		
-		console.log("total: "+width);
-		$('.center-main').not('.navlinks').width(width);
-	});
+	$(window).resize(setDimensions);
 
 	AutoloadTemplates(); 
 	var id = GetUrlParam('id');
 	AutoloadDataTemplates(id);
+	
+	var url = window.location.pathname;
+	var filename = url.substring(url.lastIndexOf('/')+1);
+	$(window).on("loadscripts", function(){
+		setDimensions();
+		loadImages();
+	
+		$('.navlinks a[href="'+filename+'"]').addClass('current');
+		$('.copyright span').text((new Date()).getFullYear());
+		FormatDates($('[data-format="date"]'));
+		FormatDates($('[data-format="datetime"]'), true);
+	});
+	
+	$('.load-more').click(function(){
+		var pageNum = parseInt($(this).prev().attr("data-page")) + 1;
+		$(this).prev().attr("data-page", pageNum);
+		LoadDataTemplate($(this).prev());
+	})
 });
+
+function setDimensions()
+{
+	var width = 0; 
+	$('.navlinks a').each(function(i, ele){
+		var textW = $(ele).width();
+		var marginW = parseFloat($(ele).css("margin-right").replace("%", "").replace("px", ""));
+		var marginL = parseFloat($(ele).css("margin-left").replace("%", "").replace("px", ""));
+		width += textW + ($('body').width() * ((marginW + marginL)/100.0));
+	});
+	
+	$('.center-main').not('.navlinks').outerWidth(width + 15);
+	
+	$('.ratio').each(function(i, ele){
+		var ratio = parseFloat($(ele).attr("rel"));
+
+		if(!isNaN(ratio)){
+			$(ele).height($(ele).width() * ratio);
+		}
+	});
+
+}
+
+function loadImages()
+{
+	var images = new Array()
+	var colorImages = new Array();
+	for (i = 1; i < 8; i++) { //TODO: GET NUMBER FROM DATABASE
+		$('.hidden2').show();
+		$('.hidden2').css("background-image",'url("Images/Projects/' + i + '_color.png")');
+		$('.hidden2').hide();
+	}
+}
 
 function PushPromise()
 {
 	ajaxLoadStack.push(ajaxLoadStack.length);
 }
+
 function PopPromise()
 {
 	if(ajaxLoadStack.length > 0)
@@ -61,6 +99,8 @@ function LoadDataTemplate(container, id)
 	var tok = sessionStorage.getItem("tolkien");
 	
 	var dataType = $(container).attr("rel");
+	var pageNum = $(container).attr("data-page");
+	var extraParam = $(container).attr("data-param");
 	var dataEndpoint = dataType.charAt(0).toUpperCase() + dataType.substr(1).toLowerCase();
 	var postData = {"func":"get"};
 	if(authId && tok)
@@ -68,10 +108,17 @@ function LoadDataTemplate(container, id)
 		postData["authId"] = authId;
 		postData["auth"] = tok;
 	}
+	if(pageNum)
+	{
+		postData["page"] = pageNum;
+		postData["size"] = defaultPageSize;
+	}
 	if(id) postData["id"] = id;
+	if(extraParam) postData["param"] = extraParam;
 	
 	var templateEndpoint = GetLocalUrl("Templates/"+dataType+(id?"":"s")+".html");
 	
+	PushPromise();
 	Ajax(dataEndpoint, postData, function(data){
 	$.get(templateEndpoint, function(template) {
 		if(id)
@@ -90,6 +137,9 @@ function LoadDataTemplate(container, id)
 		}
 		if(ajaxLoadStack.length)
 			PopPromise();
+			
+		if(pageNum && $(data).length < defaultPageSize)
+			$(container).next('.load-more').remove();	
 	});});
 }
 
@@ -132,7 +182,7 @@ function Ajax(object, dataSet, callback, errorSelector)
 	  	}
 	  }
 	}).done(function(data){
-		//console.log(data);
+
 		if(!data || data.length <= 0)
 		{
 			callback(false);
@@ -207,3 +257,35 @@ function DestroyCookie()
 	document.cookie = "bucketofsnow=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
 }
 
+function FormatDates(containers, useTime)
+{
+	$(containers).each(function(idx, d){
+		if(!$(d).text().includes(":"))
+			return true;
+			
+		var date = new Date($(d).text());
+		var day = date.getDate().toString();
+		var month = date.toLocaleString("en-us", { month: "long" });
+		var lastDigit = parseInt(day.substr(day.length - 1));
+		var superscript = "th";
+		if( lastDigit < 3 )
+			superscript = (lastDigit == 1)? "st" : "nd";
+		var year = date.getFullYear();
+		var hours = date.getHours();
+		var ampm = hours >= 12 ? 'pm' : 'am';
+  		var minutes = date.getMinutes();
+  		
+  		hours = hours % 12;
+  		if(hours == 0) hours = 12;
+  		if(hours < 10) hours = "0"+hours;
+  		if(minutes < 10) minutes = "0"+minutes;
+  		//if(day < 10) day = "0"+day;
+  		if(month < 10) month = "0"+month;
+  		
+		var formatted = month + " " + day + superscript +", " +year;
+		if(useTime)
+			formatted += "&emsp;" + hours + ":" + minutes + " "+ ampm;
+			
+		$(d).html(formatted);
+	});
+}
